d569aa3a9ff27667f1b6fe64939716950529c6e3
j <j@j.j>
2024-04-03 18:49:17 -0400

    search api

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -21,14 +21,12 @@ router.get('/', async (req, res, next) => {
         // Task 3: Add other filters to the query
         if (req.query.category) {
             query.category = req.query.category;
-            query.condition = req.query.condition;
-            query.age_years = { $lte: parseInt(req.query.age_years) };
+            
         }
         if (req.query.condition) {
-            // {{insert code here}} 
+            query.condition = req.query.condition;
         }
         if (req.query.age_years) {
-            // {{insert code here}}
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
